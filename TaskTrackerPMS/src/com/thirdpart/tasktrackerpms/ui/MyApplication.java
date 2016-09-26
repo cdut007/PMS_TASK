5cf68203f9b20eaafce7c46c8efd3ab89f995884
lrobot <lrobot.qq@gmail.com>
2016-09-26 11:30:46 +0800

    add the upgrade version info.

--- TaskTrackerPMS/src/com/thirdpart/tasktrackerpms/ui/MyApplication.java
+++ TaskTrackerPMS/src/com/thirdpart/tasktrackerpms/ui/MyApplication.java
@@ -10,6 +10,7 @@ import org.apache.log4j.Level;
 import android.app.Application;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.os.Environment;
 import cn.jpush.android.api.JPushInterface;
 
 import com.jameschen.comm.utils.Log;
@@ -20,6 +21,8 @@ import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
 import com.squareup.otto.Bus;
+import com.tencent.bugly.Bugly;
+import com.tencent.bugly.beta.Beta;
 import com.thirdpart.model.TestReq;
 import com.thirdpart.tasktrackerpms.R;
 
@@ -50,9 +53,77 @@ public class MyApplication extends Application {
         JPushInterface.setDebugMode(true); 	// 设置开启日志,发布时请关闭日志
         JPushInterface.init(this);     		// 初始化 JPush
         TestReq.init(this);
+        initUpgrade(this);
 	}
 
 	
+	private void initUpgrade(MyApplication myApplication) {
+	    /**** Beta高级设置*****/
+        /**
+         * true表示app启动自动初始化升级模块；
+         * false不好自动初始化
+         * 开发者如果担心sdk初始化影响app启动速度，可以设置为false
+         * 在后面某个时刻手动调用
+         */
+        Beta.autoInit = true;
+
+        /**
+         * true表示初始化时自动检查升级
+         * false表示不会自动检查升级，需要手动调用Beta.checkUpgrade()方法
+         */
+        Beta.autoCheckUpgrade = true;
+
+        /**
+         * 设置升级周期为60s（默认检查周期为0s），60s内SDK不重复向后天请求策略
+         */
+        Beta.initDelay = 1 * 1000;
+
+        /**
+         * 设置通知栏大图标，largeIconId为项目中的图片资源；
+         */
+        Beta.largeIconId = R.drawable.logo;
+
+        /**
+         * 设置状态栏小图标，smallIconId为项目中的图片资源id;
+         */
+        Beta.smallIconId = R.drawable.logo;
+
+
+        /**
+         * 设置更新弹窗默认展示的banner，defaultBannerId为项目中的图片资源Id;
+         * 当后台配置的banner拉取失败时显示此banner，默认不设置则展示“loading“;
+         */
+        Beta.defaultBannerId = R.drawable.logo;
+
+        /**
+         * 设置sd卡的Download为更新资源保存目录;
+         * 后续更新资源会保存在此目录，需要在manifest中添加WRITE_EXTERNAL_STORAGE权限;
+         */
+        Beta.storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+
+        /**
+         * 点击过确认的弹窗在APP下次启动自动检查更新时会再次显示;
+         */
+        Beta.showInterruptedStrategy = true;
+
+        /**
+         * 只允许在MainActivity上显示更新弹窗，其他activity上不显示弹窗;
+         * 不设置会默认所有activity都可以显示弹窗;
+         */
+        Beta.canShowUpgradeActs.add(MainActivity.class);
+
+        /**
+         * 已经接入Bugly用户改用上面的初始化方法,不影响原有的crash上报功能;
+         * init方法会自动检测更新，不需要再手动调用Beta.checkUpdate(),如需增加自动检查时机可以使用Beta.checkUpdate(false,false);
+         * 参数1： applicationContext
+         * 参数2：appId
+         * 参数3：是否开启debug
+         */
+        Bugly.init(getApplicationContext(),"8627b7cc8e", true);
+		
+	}
+
+
 	private void initializeImageLoader(MyApplication myApplication) {
 		// TODO Auto-generated method stub
 		ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(myApplication).threadPriority(Thread.NORM_PRIORITY - 2)
